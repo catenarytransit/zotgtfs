327e4edc5b944013038240f6d888ecfdb86ecd20
Nebula3lem123 <whitecottonjacob00@gmail.com>
2024-04-04 23:33:01 -0600

    Added a few more tests and some comments

--- src/lib.rs
+++ src/lib.rs
@@ -36,19 +36,22 @@ struct AnteaterExpressData {
     vehicle_id: i16,
 }
 
+/*
+Parses the AnteaterExpress feed, in the form of a string, 
+into a struct, or returns an error if the string provided 
+is invalid.
+ */
 fn parse_data(data: String) -> Result<Vec<AnteaterExpressData>, Box<dyn Error + Send + Sync>> {
     let prefix_index = data.find("(");
     let suffix_index = data.chars().rev().position(|x| x == ')');
     if let (Some(prefix_index), Some(suffix_index)) = (prefix_index, suffix_index) {
         let suffix_index = data.len() - (suffix_index + 1);
         let prefix_index = prefix_index + 1;
-        println!("{}, {}", prefix_index, suffix_index);
         let data: String = data
             .chars()
             .skip(prefix_index)
             .take(suffix_index - prefix_index)
             .collect();
-        println!("{}", data);
         let data: Vec<AnteaterExpressData> = from_str(&data)?;
         Ok(data)
     } else {
@@ -64,7 +67,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_data() {
+    fn parse_data_is_ok() {
         let example_data = String::from(
             r#"jQuery111104379215856036027_1712182850874( 
             [
@@ -125,5 +128,83 @@ mod tests {
         );
         let deserialized_data = parse_data(example_data);
         assert!(deserialized_data.is_ok());
+    }
+
+    #[test]
+    fn parse_data_is_correct() {
+        let example_data = String::from(
+            r#"jQuery111104379215856036027_1712182850874( 
+            [
+                {
+                    "GroundSpeed":10.99901573793,
+                    "Heading":78,
+                    "IsDelayed":false,
+                    "IsOnRoute":true,
+                    "Latitude":33.64704,
+                    "Longitude":-117.82938,
+                    "Name":"AE02",
+                    "RouteID":4,
+                    "Seconds":3,
+                    "TimeStamp":"\/Date(1712229203000-0600)\/",
+                    "VehicleID":3
+                },
+                {
+                    "GroundSpeed":18.79921258116,
+                    "Heading":289,
+                    "IsDelayed":false,
+                    "IsOnRoute":true,
+                    "Latitude":33.6478503,
+                    "Longitude":-117.8260522,
+                    "Name":"AE04",
+                    "RouteID":4,
+                    "Seconds":2,
+                    "TimeStamp":"\/Date(1712229204000-0600)\/",
+                    "VehicleID":5
+                },
+                {
+                    "GroundSpeed":0.10066213305,
+                    "Heading":0,
+                    "IsDelayed":false,
+                    "IsOnRoute":true,
+                    "Latitude":33.6489682,
+                    "Longitude":-117.839556,
+                    "Name":"AE11",
+                    "RouteID":4,
+                    "Seconds":2,
+                    "TimeStamp":"\/Date(1712229204000-0600)\/",
+                    "VehicleID":10
+                },
+                {
+                    "GroundSpeed":0,
+                    "Heading":0,
+                    "IsDelayed":false,
+                    "IsOnRoute":true,
+                    "Latitude":33.6489278979,
+                    "Longitude":-117.8448493721,
+                    "Name":"AE08",
+                    "RouteID":5,
+                    "Seconds":2,
+                    "TimeStamp":"\/Date(1712229204000-0600)\/",
+                    "VehicleID":7
+                }
+            ] 
+        );"#,
+        );
+        let deserialized_data = match parse_data(example_data) {
+            Ok(x) => x,
+            _ => return,
+        };
+        let vehicle_0 = match deserialized_data.get(0) {
+            Some(x) => x,
+            _ => return,
+        };
+        let vehicle_2 = match deserialized_data.get(2) {
+            Some(x) => x,
+            _ => return,
+        };
+
+        assert_eq!(vehicle_0.ground_speed, 10.99901573793);
+        assert_eq!(vehicle_2.is_delayed, false);
+
     }
 }
