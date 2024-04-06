4e1d615e6bd88e67f6f4434df5e7a7022eb39968
Nebula3lem123 <whitecottonjacob00@gmail.com>
2024-04-06 12:27:51 -0600

    testing of everything except reqwest complete, run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+use gtfs_rt::vehicle_position::*;
+use gtfs_rt::*;
 use serde::Deserialize;
 use serde_json::from_str;
 use std::error::Error;
 use std::time::SystemTime;
-use gtfs_rt::*;
-use gtfs_rt::vehicle_position::*;
 
-pub async fn get_gtfs_rt() -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::Error + Send + Sync>> {
-    // steps:
+/**
+ * Fetches jsonp data from ucirvine's transit feed and converts it into gtfs_rt
+ */
+pub async fn get_gtfs_rt() -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::Error + Send + Sync>>
+{
     let data = reqwest::get("https://ucirvine.transloc.com/Services/JSONPRelay.svc/GetMapVehiclePoints?method=jQuery111104379215856036027_1712182850874&ApiKey=8882812681&_=1712182850877")
         .await?
         .text()
@@ -14,16 +17,36 @@ pub async fn get_gtfs_rt() -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::E
     return gtfs_rt_from_string(data);
 }
 
-fn gtfs_rt_from_string(data: String) -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::Error + Send + Sync>> {
+/**
+ * Function creates gtfs from a string, called by get_gtfs_rt and used for testing.
+ */
+fn gtfs_rt_from_string(
+    data: String,
+) -> Result<gtfs_rt::FeedMessage, Box<dyn std::error::Error + Send + Sync>> {
     let data = parse_data(data)?;
+    // if data parsed is empty (at night for example) returns an empty gtfs_rt feed.
+    if data.len() == 0 {
+        let emtpy_entity: Vec<gtfs_rt::FeedEntity> = Vec::new();
+        let empty_data = gtfs_rt::FeedMessage {
+            header: FeedHeader {
+                gtfs_realtime_version: String::from("2.0"),
+                incrementality: None,
+                timestamp: None,
+            },
+            entity: emtpy_entity,
+        };
+        return Ok(empty_data);
+    }
     let mut anteater_entities: Vec<FeedEntity> = Vec::new();
     for i in 0..data.len() {
         let vehicle = match data.get(i) {
             Some(x) => x,
-            None => return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Invalid String"
-            ))),
+            None => {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Invalid String",
+                )))
+            }
         };
         anteater_entities.push(FeedEntity {
             id: i.to_string(),
@@ -36,17 +59,18 @@ fn gtfs_rt_from_string(data: String) -> Result<gtfs_rt::FeedMessage, Box<dyn std
     }
     let anteater_gtfs = gtfs_rt::FeedMessage {
         header: FeedHeader {
-            gtfs_realtime_version: "2.0".to_string(),
+            gtfs_realtime_version: String::from("2.0"),
             incrementality: Some(1),
-            timestamp: Some(SystemTime::now()
-                .elapsed()?
-                .as_secs()),
+            timestamp: Some(SystemTime::now().elapsed()?.as_secs()),
         },
         entity: anteater_entities.to_owned(),
     };
     return Ok(anteater_gtfs);
 }
 
+/**
+ * Struct represents one vehicle in the jsonp provided by ucirvine.
+ */
 #[derive(Deserialize)]
 struct AnteaterExpressData {
     #[serde(rename = "GroundSpeed")]
@@ -65,8 +89,17 @@ struct AnteaterExpressData {
     vehicle_id: i16,
 }
 
+/**
+ * Implementation block of AnteaterExpressData. Functions
+ * generate pieces of gtfs_rt data to be used in
+ * gtfs_rt_from_string.
+ */
 impl AnteaterExpressData {
-
+    /**
+     * Generates gtfs_rt::CarriageDetails based on self's data.
+     * gtfs_data assumes all vehicles are non-articulated (as
+     * of now they are not/no carriage details are provided).
+     */
     fn get_carriage_details(&self) -> Result<CarriageDetails, Box<dyn Error + Send + Sync>> {
         return Ok(CarriageDetails {
             id: Some(self.vehicle_id.clone().to_string()),
@@ -77,6 +110,9 @@ impl AnteaterExpressData {
         });
     }
 
+    /**
+     * Generates a gtfs_rt::Position based on self's data.
+     */
     fn get_position(&self) -> Result<Position, Box<dyn Error + Send + Sync>> {
         return Ok(Position {
             latitude: self.latitude,
@@ -87,9 +123,12 @@ impl AnteaterExpressData {
         });
     }
 
+    /**
+     * Generates a gtfs_rt::TripDescriptor based on self's data.
+     */
     fn get_trip_descriptor(&self) -> Result<TripDescriptor, Box<dyn Error + Send + Sync>> {
         return Ok(TripDescriptor {
-            trip_id: Some("".to_string()),
+            trip_id: Some(String::from("")),
             route_id: Some(self.route_id.clone().to_string()),
             direction_id: Some(0),
             start_time: None,
@@ -98,46 +137,60 @@ impl AnteaterExpressData {
         });
     }
 
+    /**
+     * Generates a gtfs_rt::VehicleDescriptor based on self's
+     * data.
+     */
     fn get_vehicle_descriptor(&self) -> Result<VehicleDescriptor, Box<dyn Error + Send + Sync>> {
         return Ok(VehicleDescriptor {
             id: Some(self.vehicle_id.to_string().clone()),
             label: Some(self.name.clone()),
             license_plate: None,
             wheelchair_accessible: None,
-        })
+        });
     }
 
+    /**
+     * Generates a gtfs_rt::VehiclePosition based on self's
+     * data and the impl functions above.
+     */
     fn get_vehicle_position(&self) -> Result<VehiclePosition, Box<dyn Error + Send + Sync>> {
         return Ok(VehiclePosition {
             trip: Some(self.get_trip_descriptor()?),
             vehicle: Some(self.get_vehicle_descriptor()?),
             position: Some(self.get_position()?),
             current_stop_sequence: None, //fetch from gtfs
-            stop_id: None, //fetch from gtfs
+            stop_id: None,               //fetch from gtfs
             current_status: None,
             timestamp: None,
             congestion_level: None,
             occupancy_status: None,
             occupancy_percentage: None,
-            multi_carriage_details: vec![self.get_carriage_details()?],
+            multi_carriage_details: vec![self.get_carriage_details()?], //Length of 1 since anteater express does not provide carriage details
         });
     }
 }
 
-/*
-Parses the AnteaterExpress feed, in the form of a string, 
-into a struct, or returns an error if the string provided 
-is invalid.
+/**
+ * Parses the AnteaterExpress feed, in the form of a string,
+ * into a struct, or returns an error if the string provided
+ * is invalid.
  */
 fn parse_data(data: String) -> Result<Vec<AnteaterExpressData>, Box<dyn Error + Send + Sync>> {
-    let prefix_index = data.find("("); 
+    let prefix_index = data.find("(");
     let suffix_index = data.chars().rev().position(|x| x == ')');
     if let (Some(prefix_index), Some(suffix_index)) = (prefix_index, suffix_index) {
         //index at which data starts
         let suffix_index = data.len() - (suffix_index + 1);
         //index at which data ends
         let prefix_index = prefix_index + 1;
+
+        if suffix_index - prefix_index < 100 {
+            let nothing: Vec<AnteaterExpressData> = Vec::new();
+            return Ok(nothing);
+        }
         // data is iterated through, skips up to prefix_index, takes up to suffix_index, and collects the values
+
         let data: String = data
             .chars()
             .skip(prefix_index)
@@ -153,149 +206,171 @@ fn parse_data(data: String) -> Result<Vec<AnteaterExpressData>, Box<dyn Error + 
     }
 }
 
+/**
+ * Test block
+ */
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn parse_data_is_ok() {
-        let example_data = String::from(
+    fn create_example_string() -> String {
+        String::from(
             r#"jQuery111104379215856036027_1712182850874( 
+                [
+                    {
+                        "GroundSpeed":10.99901573793,
+                        "Heading":78,
+                        "IsDelayed":false,
+                        "IsOnRoute":true,
+                        "Latitude":33.64704,
+                        "Longitude":-117.82938,
+                        "Name":"AE02",
+                        "RouteID":4,
+                        "Seconds":3,
+                        "TimeStamp":"\/Date(1712229203000-0600)\/",
+                        "VehicleID":3
+                    },
+                    {
+                        "GroundSpeed":18.79921258116,
+                        "Heading":289,
+                        "IsDelayed":false,
+                        "IsOnRoute":true,
+                        "Latitude":33.6478503,
+                        "Longitude":-117.8260522,
+                        "Name":"AE04",
+                        "RouteID":4,
+                        "Seconds":2,
+                        "TimeStamp":"\/Date(1712229204000-0600)\/",
+                        "VehicleID":5
+                    },
+                    {
+                        "GroundSpeed":0.10066213305,
+                        "Heading":0,
+                        "IsDelayed":false,
+                        "IsOnRoute":true,
+                        "Latitude":33.6489682,
+                        "Longitude":-117.839556,
+                        "Name":"AE11",
+                        "RouteID":4,
+                        "Seconds":2,
+                        "TimeStamp":"\/Date(1712229204000-0600)\/",
+                        "VehicleID":10
+                    },
+                    {
+                        "GroundSpeed":0,
+                        "Heading":0,
+                        "IsDelayed":false,
+                        "IsOnRoute":true,
+                        "Latitude":33.6489278979,
+                        "Longitude":-117.8448493721,
+                        "Name":"AE08",
+                        "RouteID":5,
+                        "Seconds":2,
+                        "TimeStamp":"\/Date(1712229204000-0600)\/",
+                        "VehicleID":7
+                    }
+                ] 
+            );"#,
+        )
+    }
+
+    fn create_no_data_string() -> String {
+        String::from("jQuery111104379215856036027_1712182850874(
             [
                 {
-                    "GroundSpeed":10.99901573793,
-                    "Heading":78,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.64704,
-                    "Longitude":-117.82938,
-                    "Name":"AE02",
-                    "RouteID":4,
-                    "Seconds":3,
-                    "TimeStamp":"\/Date(1712229203000-0600)\/",
-                    "VehicleID":3
-                },
-                {
-                    "GroundSpeed":18.79921258116,
-                    "Heading":289,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6478503,
-                    "Longitude":-117.8260522,
-                    "Name":"AE04",
-                    "RouteID":4,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":5
-                },
-                {
-                    "GroundSpeed":0.10066213305,
-                    "Heading":0,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6489682,
-                    "Longitude":-117.839556,
-                    "Name":"AE11",
-                    "RouteID":4,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":10
-                },
-                {
-                    "GroundSpeed":0,
-                    "Heading":0,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6489278979,
-                    "Longitude":-117.8448493721,
-                    "Name":"AE08",
-                    "RouteID":5,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":7
                 }
             ] 
-        );"#,
-        );
+        )")
+    }
+
+    #[test]
+    fn parse_data_is_ok() {
+        let example_data = create_example_string();
         let deserialized_data = parse_data(example_data);
         assert!(deserialized_data.is_ok());
     }
 
     #[test]
     fn parse_data_is_correct() {
-        let example_data = String::from(
-            r#"jQuery111104379215856036027_1712182850874( 
-            [
-                {
-                    "GroundSpeed":10.99901573793,
-                    "Heading":78,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.64704,
-                    "Longitude":-117.82938,
-                    "Name":"AE02",
-                    "RouteID":4,
-                    "Seconds":3,
-                    "TimeStamp":"\/Date(1712229203000-0600)\/",
-                    "VehicleID":3
-                },
-                {
-                    "GroundSpeed":18.79921258116,
-                    "Heading":289,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6478503,
-                    "Longitude":-117.8260522,
-                    "Name":"AE04",
-                    "RouteID":4,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":5
-                },
-                {
-                    "GroundSpeed":0.10066213305,
-                    "Heading":0,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6489682,
-                    "Longitude":-117.839556,
-                    "Name":"AE11",
-                    "RouteID":4,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":10
-                },
-                {
-                    "GroundSpeed":0,
-                    "Heading":0,
-                    "IsDelayed":false,
-                    "IsOnRoute":true,
-                    "Latitude":33.6489278979,
-                    "Longitude":-117.8448493721,
-                    "Name":"AE08",
-                    "RouteID":5,
-                    "Seconds":2,
-                    "TimeStamp":"\/Date(1712229204000-0600)\/",
-                    "VehicleID":7
-                }
-            ] 
-        );"#,
-        );
-        let deserialized_data = match parse_data(example_data) {
-            Ok(x) => x,
-            _ => return,
-        };
-        let vehicle_0 = match deserialized_data.get(0) {
-            Some(x) => x,
-            _ => return,
-        };
-        let vehicle_2 = match deserialized_data.get(2) {
-            Some(x) => x,
-            _ => return,
-        };
+        let example_data = create_example_string();
+        let deserialized_data = parse_data(example_data).unwrap();
+        let vehicle_0 = deserialized_data.get(0).unwrap();
+        let vehicle_2 = deserialized_data.get(2).unwrap();
 
         assert_eq!(vehicle_0.ground_speed, 10.99901573793);
         assert_eq!(vehicle_2.vehicle_id, 10);
+    }
 
+    #[test]
+    fn parse_data_no_data() {
+        let example_data = create_no_data_string();
+        let deserialized_data = parse_data(example_data);
+        assert!(deserialized_data.is_ok());
+    }
+
+    #[test]
+    fn get_gtfs_rt_no_data() {
+        let example_data = create_no_data_string();
+        let anteater_gtfs = gtfs_rt_from_string(example_data);
+        assert!(anteater_gtfs.is_ok());
+    }
+
+    #[test]
+    fn get_gtfs_rt_is_ok() {
+        let example_data = create_example_string();
+        let anteater_gtfs = gtfs_rt_from_string(example_data);
+        assert!(anteater_gtfs.is_ok());
+    }
+
+    #[test]
+    fn get_gtfs_rt_same_length_as_express_data() {
+        let example_data = create_example_string();
+        let anteater_data = parse_data(example_data).unwrap();
+        let example_data = create_example_string();
+        let anteater_gtfs = gtfs_rt_from_string(example_data).unwrap();
+        assert_eq!(anteater_data.len(), anteater_gtfs.entity.len());
+    }
+
+    #[test]
+    fn get_gtfs_rt_is_correct() {
+        let example_data = create_example_string();
+        let anteater_data = gtfs_rt_from_string(example_data).unwrap();
+        let entity_1 = anteater_data.entity.get(1).unwrap();
+        let expected_heading: f32 = 289.0;
+        let entity_3 = anteater_data.entity.get(3).unwrap();
+        let entity_0 = anteater_data.entity.get(0).unwrap();
+        assert_eq!(
+            entity_1
+                .clone()
+                .vehicle
+                .unwrap()
+                .position
+                .unwrap()
+                .bearing
+                .unwrap(),
+            expected_heading
+        );
+        assert_eq!(
+            entity_3
+                .clone()
+                .vehicle
+                .unwrap()
+                .vehicle
+                .unwrap()
+                .id
+                .unwrap(),
+            String::from("7")
+        );
+        assert_eq!(
+            entity_0
+                .clone()
+                .vehicle
+                .unwrap()
+                .trip
+                .unwrap()
+                .route_id
+                .unwrap(),
+            String::from("4")
+        );
     }
 }
